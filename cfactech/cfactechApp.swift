04b3af1147be2b5540780f27e6a832645e0bfe8f
yeurysl <yeurysl17@gmail.com>
2025-02-10 15:52:55 -0500

    it commit

---
+++ cfactech/cfactechApp.swift
@@ -0,0 +1,17 @@
+//
+//  cfactechApp.swift
+//  cfactech
+//
+//  Created by Yeurys Lora on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct cfactechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
